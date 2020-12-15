cfdbefc9cd46ddd6b7cc37793d617061e0a2381f
Dairon Leyva <dleyva@peopleticker.com>
2020-12-14 20:27:50 -0500

    prod config

--- webserver/start.py
+++ webserver/start.py
@@ -1,14 +1,18 @@
 import os
 import uvicorn
 
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     uvicorn.run(
         "webserver.app:app",
-        host="127.0.0.1",
-        port=5000,
-        log_level="debug",
+        host="127.0.0.1" if DEBUG else "0.0.0.0",
+        port=5000 if DEBUG else 80,
+        log_level="debug" if DEBUG else "info",
+        debug=DEBUG,
+        root_path=BASE_DIR,
         env_file=f"{BASE_DIR}/webserver/env.dev",
-        reload=True
+        reload=DEBUG
     )
